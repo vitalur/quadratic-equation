462bb715afb23912c6a6501c14cf94618528e49c
vitalur <39513998+vitalur@users.noreply.github.com>
2018-09-19 22:46:32 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let array = equation.split(' ');
+
+  let determinant = 0;
+  if (array[7] === '-') {
+  	determinant = Math.pow(array[4], 2) + 4 * array[0] * array[8];
+  } else {
+ 	determinant = Math.pow(array[4], 2) - 4 * array[0] * array[8];
+  }
+  let x1 = 0, x2 = 0;
+  if (array[3] === '-') {
+  	x1 = (parseInt(array[4]) + parseInt(Math.sqrt(determinant))) / (2 * array[0]); 
+  	x2 = (array[4] - Math.sqrt(determinant)) / (2 * array[0]); 
+  } else {
+  	x1 = (-parseInt(array[4]) + parseInt(Math.sqrt(determinant))) / (2 * array[0]); 
+  	x2 = (-array[4] - Math.sqrt(determinant)) / (2 * array[0]); 
+  }
+  return [x1.toFixed(), x2.toFixed()].sort(function(a, b) {return a - b}) ;
 }
